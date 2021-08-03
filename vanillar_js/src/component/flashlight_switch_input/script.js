053e0fab33768bba5dc778b65c85f856d8e10c5a
khoiqt <khoiqt>
2021-08-04 05:55:41 +0700

    update flashlight switch input

---
+++ vanillar_js/src/component/flashlight_switch_input/script.js
@@ -0,0 +1,12 @@
+var flashlightSwitchInputList = document.querySelectorAll('.flashlight-switch-input');
+
+flashlightSwitchInputList.forEach(function (flashlightSwitchInput) {
+    let button = flashlightSwitchInput.querySelector('.flashlight-switch-input__button');
+    let input = flashlightSwitchInput.querySelector('.flashlight-switch-input__input');
+    button.onclick = function () {
+        flashlightSwitchInput.classList.toggle('--is-active');
+        let inputType = (flashlightSwitchInput.classList.contains('--is-active')) ? 'text' : 'password';
+        console.log(inputType);
+        input.setAttribute('type', inputType);
+    }
+})
